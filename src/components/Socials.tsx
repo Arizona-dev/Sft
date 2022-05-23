54f1ccb4f6af7659ed73f43b875b86cb50b26321
Naderi Farid <geekwid@gmail.com>
2022-05-23 13:11:54 +0200

    [fix] added twitter social button

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Socials: React.FC = () => {
   return (
-    <div className="mx-auto color-dark-text font-ibm-plex-mono">
-      <div className="md:flex py-20 mx-6 md:mx-10 md:px-0 lg:py-32" id="team">
+    <div className="mx-auto color-dark-text">
+      <div className="md:flex py-20 mx-6 md:mx-10 md:px-0 lg:py-32">
         <div className="md:w-1/2 lg:text-right md:flex lg:flex-row-reverse lg:pr-24 relative">
           <h3 className="pb-5 md:pb-0 text-xl md:text-2xl lg:text-4xl lg:ml-44 font-syne font-bold">
             THE <span className="lg:w-full md:block">TEAM</span>
@@ -11,14 +11,22 @@ const Socials: React.FC = () => {
         </div>
         <div className="md:w-1/2 text-base grid grid-cols-2 lg:grid-cols-3 gap-5 max-w-3xl">
           <div className="">
-            <a href="https://twitter.com/themaze_eth" target="_blank" rel="noreferrer">
+            <a
+              href="https://twitter.com/themaze_eth"
+              target="_blank"
+              rel="noreferrer"
+            >
               <img src="/assets/images/team/themaze.png" alt="sft_maze" />
               <p className="font-syne font-bold uppercase pt-5">@themaze_eth</p>
             </a>
             <p className=" text-sm">Artist</p>
           </div>
           <div className="">
-            <a href="https://twitter.com/Zhenzhe_" target="_blank" rel="noreferrer">
+            <a
+              href="https://twitter.com/Zhenzhe_"
+              target="_blank"
+              rel="noreferrer"
+            >
               <img src="/assets/images/team/zhen.png" alt="sft_zhen" />
               <p className="font-syne font-bold uppercase pt-5">@Zhenzhe_</p>
             </a>
@@ -27,32 +35,48 @@ const Socials: React.FC = () => {
         </div>
       </div>
       <hr></hr>
-      <div className="py-20 mx-6 md:mx-10 md:px-0 lg:py-32">
-        <div className="md:text-center relative">
-          <div className="w-full md:flex lg:block md:px-10">
-            <div className="md:w-1/2 lg:w-full md:text-left">
-              <h3 className="text-xl md:text-4xl lg:text-center font-bold md:mx-auto">
-                JOIN OUR DISCORD
-                <span className="lg:w-full lg:block"> COMMUNITY</span>
-              </h3>
-            </div>
-            <div className="md:w-1/2 lg:w-full md:text-left lg:text-center">
-              <p className="lg:mt-10 md:mx-auto lg:w-1/3 mb-10">
-                Join our Discord server to keep up to date with Solana Funky
-                Trolls!
-              </p>
+      <div className="mx-auto color-dark-text">
+        <div className="md:flex py-20 mx-6 md:mx-10 md:px-0 lg:py-32">
+          <div className="md:w-1/2 lg:text-right md:flex lg:flex-row-reverse lg:pr-24 relative">
+            <h3 className="pb-5 md:pb-0 text-xl md:text-2xl lg:text-4xl lg:ml-44 font-syne font-bold">
+              JOIN OUR
+              <span className="lg:w-full md:block"> COMMUNITY</span>
+            </h3>
+          </div>
+          <div className="md:w-1/2 text-left">
+            <p className="md:mx-auto mb-10">
+              Join our Discord server and follow us on Twitter to keep up to
+              date with Solana Funky Trolls!
+            </p>
+            <div className="flex flex-row space-x-4 text-center">
               <a
                 href="https://discord.com/invite/eQWZ9c7Wbt"
                 target="_blank"
                 rel="noreferrer"
-                className="button-sft bg-custom-black rounded-lg text-white p-5 px-10 md:px-15 inline-block"
+                className="button-sft bg-custom-black rounded-lg text-white p-5 w-48"
               >
                 <img
                   src="assets/images/discord-white.svg"
                   className="w-5 inline-block"
                   alt="discord logo"
                 />
-                <span className="pl-3 text-sm font-medium">Join Discord</span>
+                <span className="pl-3 text-xs md:text-sm font-medium">Join Discord</span>
+              </a>
+              <a
+                href="https://discord.com/invite/eQWZ9c7Wbt"
+                target="_blank"
+                rel="noreferrer"
+                className="button-sft bg-custom-black rounded-lg text-white p-5 w-48"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 24 24"
+                  className="w-5 inline-block"
+                >
+                  <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
+                </svg>
+                <span className="pl-3 text-xs md:text-sm font-medium">Follow Us</span>
               </a>
             </div>
           </div>
